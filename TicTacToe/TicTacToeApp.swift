90b80d5a15a7f6dbee0f3247d8385063b03e92ce
bilrdi <billiumardaeli@gmail.com>
2022-09-01 08:41:55 +0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Billi Umar Daeli on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
